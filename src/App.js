546adbc2342a4ea65d618e29cead346a183d7694
Noam Cohen <92931230+NoamCohen48@users.noreply.github.com>
2022-04-01 18:18:35 +0300

    removed uneccery files and added dependencies

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+// import 'bootstrap';
 
 function App() {
+  console.log()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hello World</h1>
+      <button type="button" className="btn btn-primary">Primary</button>
+      
+    </>
   );
 }
 
