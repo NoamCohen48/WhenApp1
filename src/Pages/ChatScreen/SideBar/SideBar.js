c9b161a2b9065961397dab74c74131685b2c6fdf
Noam Cohen <92931230+NoamCohen48@users.noreply.github.com>
2022-04-07 00:06:51 +0300

    started SideBar in ChatScreen

--- src/Pages/ChatScreen/SideBar/SideBar.js
+++ src/Pages/ChatScreen/SideBar/SideBar.js
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useChatContext } from '../../../Components/ContextProvider/ChatContextProvider';
 import SideBarItem from './SideBarItem';
+import './SideBar.css'
 
 function SideBar(props) {
+    const [contacts, setContacts] = useState(undefined)
+    let chatContext = useChatContext();
+
+    useEffect(() => {
+        setContacts((prevContacts) => {
+            return chatContext.contacts;
+        })
+    }, []);
 
     /*
     TODO:
     1. create component of items
     2. list as column flexbox
     3. each has function that changes the setter
+    4. fetch contacts
+    5. design top bar
     */
 
 
+    if (contacts === undefined) {
+        return (
+            <h1>
+                Loading
+            </h1>
+        )
+    }
+
     return (
         <>
-            this is the side bar
-            <br></br>
-            <SideBarItem person={"mike"} />
-            <br></br>
-            <SideBarItem person={"jhon"}  />
-
+            <div className='top-bar'>
+                <h1>User info</h1>
+            </div>
+            <div className='contacts-list'>
+                {contacts.map(username =>
+                    <SideBarItem key={username} username={username} />
+                )}
+            </div>
         </>
     )
 }
--- src/Pages/ChatScreen/base/ChatScreen.js
+++ src/Pages/ChatScreen/base/ChatScreen.js
@@ -5,18 +5,32 @@ import './ChatScreen.css'
 import DialogScreen from '../ChatDialog/DialogScreen.js';
 import SideBar from '../SideBar/SideBar.js';
 import { findPerson } from '../../../db/users';
+import { useChatContext } from '../../../Components/ContextProvider/ChatContextProvider';
 
 function ChatScreen(props) {
     let location = useLocation();
+    let chatContext = useChatContext();
     const [user, setUser] = useState(undefined);
 
     useEffect(() => {
         let username = location.state.username;
         setUser(() => {
-            return findPerson({ "username": username })[0];
+            let user = findPerson({ "username": username })[0];
+            chatContext.contacts = user.contacts;
+
+            chatContext.contacts.push("holt");
+            chatContext.contacts.push("jason");
+            chatContext.contacts.push("noam");
+            chatContext.contacts.push("shaked");
+            chatContext.contacts.push("roi");
+
+            return user;
         })
     }, [])
 
+
+    // TODO: find a solution for fetching contacts, now doing it here, putting in context and extracting in side bar
+    // Need a way to tell this parent that side bar is loading. 
 
     if (user === undefined) {
         return (
@@ -34,9 +48,6 @@ function ChatScreen(props) {
                 <div className='row'>
                     <div className="col-4 side-bar" >
                         <SideBar />
-                        {user.username}
-                        {user.img}
-                        <img src={user.img} alt="hello"></img>
                     </div>
                     <div className="col-8 dialog-screen" >
                         <DialogScreen />
