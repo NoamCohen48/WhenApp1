c92b1bf5313088fc66e5007d2e5660e9471a1272
shakedc1599 <47411973+shakedc1599@users.noreply.github.com>
2022-04-11 19:14:39 +0300

    add

--- src/Pages/ChatScreen/ChatDialog/Messages/TextMessage.js
+++ src/Pages/ChatScreen/ChatDialog/Messages/TextMessage.js
@@ -4,9 +4,9 @@ function TextMessage(props) {
     let className = props.className.concat(' ', props.message.iSent ? ' my-message' : ' other-message');
     return(
         <>
-            <div className={className}>
-                <p>{props.message.data}</p>
-            </div>
+                <div className={className}>
+                    <p>{props.message.data}</p>
+                </div>
         </>
     )
 }
